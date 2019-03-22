30ba41220d4c325af25fd8533b227b189186f6e0
Chris Morgan <chris.morgan@clearcloud.co.uk>
2019-03-22 14:13:00 +0000

    init

---
+++ variables.tf
@@ -0,0 +1,28 @@
+# ---------------------------
+# EFS Vars
+# ---------------------------
+variable "efs_encrypted" {
+  description = "Encrypt the EFS share"
+  default     = "true"
+}
+
+variable "performance_mode" {
+  description = "EFS performance mode.https://docs.aws.amazon.com/efs/latest/ug/performance.html"
+  default     = "generalPurpose"
+}
+
+variable "private_subnet_a" {
+  description = "1st private subnet id"
+}
+
+variable "private_subnet_b" {
+  description = "2nd private subnet id"
+}
+
+variable "subnet_a_ip_range" {
+  description = "1st subnet IP range, grants access to EFS mount point a"
+}
+
+variable "subnet_b_ip_range" {
+  description = "2nd subnet IP range, grants access to EFS mount point b"
+}
