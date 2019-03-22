87152a6da668abfc6837ef3f3a767c888781ec47
Chris Morgan <chris.morgan@clearcloud.co.uk>
2019-03-22 14:39:15 +0000

    added kms

--- kms.tf
+++ kms.tf
@@ -1,3 +1,15 @@
+# -------------------------------------------------------
+# KMS Key for EFS
+# -------------------------------------------------------
+resource "aws_kms_key" "efskey" {
+  description             = "This key is used to encrypt EFS in ${var.environment}"
+  deletion_window_in_days = 30
+}
+
+#
+# Alias
+#
+
 resource "aws_kms_alias" "efs" {
   name          = "alias/efs-${var.environment}"
   target_key_id = "${aws_kms_key.efskey.key_id}"
